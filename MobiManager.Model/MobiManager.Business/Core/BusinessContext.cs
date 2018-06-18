bd33a1cd79216a211ecc7e684236ed534daf0f18
cneagu <neagucristianstefan@yahoo.com>
2018-06-19 00:27:27 +0300

    adding Business layer

---
+++ MobiManager.Model/MobiManager.Business/Core/BusinessContext.cs
@@ -0,0 +1,124 @@
+﻿using MobiManager.RepositoryAbstraction.Core;
+using MobiManager.RepositoryFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobiManager.Business.Core
+{
+    public class BusinessContext : IDisposable
+    {
+        #region Members
+        private static BusinessContext _instance;
+        private IRepositoryContext _repositoryContext;
+        private DeviceAssignationBusiness _deviceAssignationBusiness;
+        private DeviceBusiness _deviceBusiness;
+        private DeviceUserBusiness _deviceUserBusiness;
+        private UserBusiness _dserBusiness;
+        #endregion
+        #region Constructor
+        public BusinessContext()
+        {
+            _instance = this;
+            _repositoryContext = Getter.GetRepository();
+        }
+        #endregion
+
+        #region Properties
+        internal static BusinessContext Current
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new Exception("No RepositoryContext instance available!");
+                }
+                return _instance;
+            }
+        }
+
+        public DeviceAssignationBusiness DeviceAssignationBusiness
+        {
+            get
+            {
+                if (_deviceAssignationBusiness == null)
+                    _deviceAssignationBusiness = new DeviceAssignationBusiness();
+                return _deviceAssignationBusiness;
+            }
+        }
+
+        public DeviceBusiness DeviceBusiness
+        {
+            get
+            {
+                if (_deviceBusiness == null)
+                    _deviceBusiness = new DeviceBusiness();
+                return _deviceBusiness;
+            }
+        }
+
+        public DeviceUserBusiness DeviceUserBusiness
+        {
+            get
+            {
+                if (_deviceUserBusiness == null)
+                    _deviceUserBusiness = new DeviceUserBusiness();
+                return _deviceUserBusiness;
+            }
+        }
+
+        public UserBusiness UserBusiness
+        {
+            get
+            {
+                if (_dserBusiness == null)
+                    _dserBusiness = new UserBusiness();
+                return _dserBusiness;
+            }
+        }
+
+        internal IRepositoryContext RepositoryContext
+        {
+            get { return _repositoryContext; }
+        }
+        #endregion
+
+        #region IDisposable Implementation
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool dispose)
+        {
+            if (dispose)
+            {
+                if (_deviceAssignationBusiness != null)
+                {
+                    _deviceAssignationBusiness = null;
+                }
+                if (_deviceBusiness != null)
+                {
+                    _deviceBusiness = null;
+                }
+                if (_deviceUserBusiness != null)
+                {
+                    _deviceUserBusiness = null;
+                }
+                if (_dserBusiness != null)
+                {
+                    _dserBusiness = null;
+                }
+            }
+        }
+
+        ~BusinessContext()
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
---
+++ MobiManager.Model/MobiManager.Business/UserBusiness.cs
@@ -0,0 +1,45 @@
+﻿using MobiManager.Business.Core;
+using MobiManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobiManager.Business
+{
+    public class UserBusiness
+    {
+        #region Methdos
+        public void Insert(User user)
+        {
+            BusinessContext.Current.RepositoryContext.UserRepository.Insert(user);
+        }
+
+        public void Update(User user)
+        {
+            BusinessContext.Current.RepositoryContext.UserRepository.Update(user);
+        }
+
+        public void Delete(Guid userID)
+        {
+            BusinessContext.Current.RepositoryContext.UserRepository.Delete(userID);
+        }
+
+        public List<User> ReadAll()
+        {
+            return BusinessContext.Current.RepositoryContext.UserRepository.ReadAll();
+        }
+
+        public User ReadByID(Guid userID)
+        {
+            return BusinessContext.Current.RepositoryContext.UserRepository.ReadByID(userID);
+        }
+
+        public string ReadUserName(string userName)
+        {
+            return BusinessContext.Current.RepositoryContext.UserRepository.ReadUserName(userName);
+        }
+        #endregion
+    }
+}
