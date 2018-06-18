dd71489a4c12ebc29e81cf3058867104573dba5b
Cristian-Stefan Neagu <stefan.neagu@fortech.ro>
2018-06-18 16:55:50 +0300

    need to add link to index

---
+++ MobiManager.Model/MobiManager.UI/Scripts/Controllers/UserController.js
@@ -0,0 +1,9 @@
+﻿var UserController = function (serviceContext) {
+    this.LogIn = function () {
+        $('#logIn').on('click', function () {
+            var firstName = $('#new-username').focusout(Function(){
+                console.log('success');
+            });
+        });
+    };
+};
--- MobiManager.Model/MobiManager.UI/Scripts/appController.js
+++ MobiManager.Model/MobiManager.UI/Scripts/appController.js
@@ -1,3 +1,10 @@
 ﻿$(document).ready(function () {
     SPA();
+    var _serviceContext = new ServiceContext();
+
+    var _userController = new UserController(_serviceContext);
+    _userController.LogIn();
+    _userController.Setings();
+    _userController.LogOut();
+    _userController.UserProfile();
 });
