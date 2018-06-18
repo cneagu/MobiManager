abb142740be894eb1ee809132680f4a42ac52d2a
cneagu <neagucristianstefan@yahoo.com>
2018-06-18 23:45:43 +0300

    implement dependency segregation principle

---
+++ MobiManager.Model/MobiManager.RepositoryFactory/Getter.cs
@@ -0,0 +1,23 @@
+﻿using MobiManager.Repository.Core;
+using MobiManager.RepositoryAbstraction.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobiManager.RepositoryFactory
+{
+    public class Getter
+    {
+        public static IRepositoryContext GetRepository()
+        {
+            bool framework = true;
+            if (framework)
+            {
+                return new RepositoryContext();
+            }
+            return default(IRepositoryContext);
+        }
+    }
+}
