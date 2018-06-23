b117dcdc46b45376ec3ceb3e1b61eb1e55c48244
cneagu <neagucristianstefan@yahoo.com>
2018-06-23 23:37:14 +0300

    asdas

--- MobiManager.Model/MobiManager.UI/Scripts/Services/DeviceAssignationService.js
+++ MobiManager.Model/MobiManager.UI/Scripts/Services/DeviceAssignationService.js
@@ -4,14 +4,14 @@
         ajaxService('DeviceAssignation/SetUser', 'POST', data ,handleData, '#user_profile');
     };
     this.RemoveUser = function (data, handleData) {
-        ajaxService('DeviceAssignation/RemoveUser', 'POST', data, handleData, '#singin');
+        ajaxService('DeviceAssignation/RemoveUser', 'POST', data, handleData, '#user_profile');
     };
 
     this.GetFreeDevice = function (data, handleData) {
-        ajaxService('DeviceAssignation/GetFreeDevice', 'GET', '', handleData, '#singin');
+        ajaxService('DeviceAssignation/GetFreeDevice', 'GET', '', handleData, '#user_profile');
     };
 
     this.ReadByID = function (data, handleData) {
-        ajaxService('DeviceAssignation/ReadByID/' + data, 'GET', '', handleData, '#singin');
+        ajaxService('DeviceAssignation/ReadByID/' + data, 'GET', '', handleData, '#user_profile');
     };
 };
--- MobiManager.Model/MobiManager.UI/Scripts/Services/UserService.js
+++ MobiManager.Model/MobiManager.UI/Scripts/Services/UserService.js
@@ -1,32 +1,32 @@
 ﻿var UserService = function () {
 
     this.ReadUserName = function (id, data, handleData) {
-        ajaxService('User/LogIn/' + data, 'GET', '', handleData, '#singin');
+        ajaxService('User/LogIn/' + data, 'GET', '', handleData, '#user_profile');
     };
 
     this.Insert = function (data, handleData) {
-        ajaxService('User/LogIn/Insert', 'POST', data, handleData, '#singin');
+        ajaxService('User/LogIn/Insert', 'POST', data, handleData, '#user_profile');
     };
 
     this.ReadLogIn = function (data, handleData) {
-        ajaxService('User/LogIn/ReadLogIn', 'POST', data, handleData, '#singin');
+        ajaxService('User/LogIn/ReadLogIn', 'POST', data, handleData, '#user_profile');
     };
 
     this.ReadById = function (data, handleData) {
-        ajaxService('User/LogIn/ReadById/' + data, 'GET', '', handleData, '#singin');
+        ajaxService('User/LogIn/ReadById/' + data, 'GET', '', handleData, '#user_profile');
     };
 
     this.Update = function (data, handleData) {
-        ajaxService('User/LogIn/Update', 'POST', data, handleData, '#singin');
+        ajaxService('User/LogIn/Update', 'POST', data, handleData, '#user_profile');
     };
     //read all
 
     this.ReadAll = function (data, handleData) {
-        ajaxService('User/ReadAll', 'GET', '', handleData, '#singin');
+        ajaxService('User/ReadAll', 'GET', '', handleData, '#user_profile');
     };
 
     //deviceUser
     this.DevicesList = function (data, handleData) {
-        ajaxService('User/DevicesList', 'POST', data, handleData, '#singin');
+        ajaxService('User/DevicesList', 'POST', data, handleData, '#user_profile');
     };
 };
